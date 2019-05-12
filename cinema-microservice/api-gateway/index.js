7be4082168a50f085c4a8307d9a51b56367bdad4
David Pereira <d_pereira@outlook.com.br>
2019-05-11 22:23:03 -0300

    Utilizando JSON Web Token

--- cinema-microservice/api-gateway/index.js
+++ cinema-microservice/api-gateway/index.js
@@ -1,3 +1,5 @@
+require('dotenv-safe').load()
+var jwt = require('jsonwebtoken')
 var http = require('http')
 const express = require('express')
 const httpProxy = require('express-http-proxy')
@@ -9,16 +11,38 @@ const helmet = require('helmet')
 const userServiceProxy = httpProxy('http://localhost:3001')
 const productsServiceProxy = httpProxy('http://localhost:3002')
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req, res, next) => {
     res.json({msg: 'Bem vindo ao Api-Gateway'})
 })
 
-app.get('/users', (req, res, next) => {
+app.get('/users', verifyJWT, (req, res, next) => {
     userServiceProxy(req, res, next)
 })
 
-app.get('/products', (req, res, next) => {
+app.get('/products', verifyJWT, (req, res, next) => {
     productsServiceProxy(req, res, next)
+})
+
+app.post('/login', (req, res, next) => {
+    console.log(req);
+    
+    if (req.body.user === 'david' && req.body.pwd === '123') {
+        const id = 1;
+        var token = jwt.sign({id}, process.env.SECRET, {
+            expiresIn: 300
+        })
+
+        res.status(200).send({auth:true, token: token})
+    }
+
+    res.status(500).send('Login inválido')
+})
+
+app.get('/logout', (req, res, next) => {
+    res.status(200).send({auth:false, token: null})
 })
 
 app.use(logger('dev'))
@@ -31,3 +55,14 @@ var server = http.createServer(app)
 server.listen(3000, () => {
     console.log('Server is running in port 3000')
 })
+
+function verifyJWT(req, res, next) {
+    var token = req.headers['x-access-token']
+    if (!token) return res.status(401).send({auth:false, message:'No token provided'})
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return res.status(500).send({auth:false, message:'Failed to authenticate token'})
+        req.userId = decoded.id
+        next()
+    })
+}
